a84c8de93fc9a3617220567c398732f9c533e693
Julio de la Matta <julio.matta@berger-levrault.com>
2022-08-26 14:31:32 +0200

    Sección 17.1

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('GIGABYTE RTX 3070 Ti GDDR6 12GB')
+  });
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
